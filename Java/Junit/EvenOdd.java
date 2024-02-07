0abe07f9d9828c609cb7cc3dc18db5eea74e68dd
SurajBargaje <surajbargaje.scoe.mechanical@gmail.com>
2024-02-07 23:49:22 +0530

    Even odd code for testing using JUnit

---
+++ Java/Junit/EvenOdd.java
@@ -0,0 +1,14 @@
+class EvenOdd {
+    public static void main(String[] args) {
+
+    }
+
+    public boolean evenOdd(int num) {
+        if (num % 2 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+}
