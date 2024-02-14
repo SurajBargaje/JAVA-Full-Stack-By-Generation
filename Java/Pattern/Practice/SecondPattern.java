1cc7f349e5e4c29b08b717012d8b6d6fbe5b2c05
SurajBargaje <surajbargaje.scoe.mechanical@gmail.com>
2024-02-14 09:15:45 +0530

    Pattern printing Practice

---
+++ Java/Pattern/Practice/SecondPattern.java
@@ -0,0 +1,12 @@
+public class SecondPattern {
+    public static void main(String[] args) {
+        
+        for (int row = 0; row <5 ; row++) {
+            for (int col = 0; col <=row ; col++) {
+             System.out.print("*");   
+            }
+            System.out.println();
+            
+        }
+    }
+}
