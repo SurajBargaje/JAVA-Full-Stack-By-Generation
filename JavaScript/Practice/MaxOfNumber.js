8c8546f5c488886f5f1824c896d9f052233a7b2e
SurajBargaje <surajbargaje.scoe.mechanical@gmail.com>
2024-03-08 17:04:50 +0530

    Javascript program to find max of two number

---
+++ JavaScript/Practice/MaxOfNumber.js
@@ -0,0 +1,13 @@
+// Write a JavaScript program to find the maximum of two numbers.
+
+let num1 = 45;
+let num2 = 10;
+if(num1>num2){
+  console.log(`${num1} is maximum `);
+}
+else if(num2>num1){
+  console.log(`${num2} is maximum `);
+}
+else{
+  console.log('Both are equal')
+}
